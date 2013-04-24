85045bab16ece617b60923506c465ac25be76112
Nicolas GAUD <ngaud@pcp-gi-42.(none)>
2013-04-24 10:52:09 +0200

    Add a new demos for the simple Agent tutorial showing just how to create two simple communicating agents

---
+++ janus/janus-demos/base-demos/helloagents/src/main/java/org/janusproject/demos/base/helloagents/AgentB.java
@@ -0,0 +1,53 @@
+package org.janusproject.demos.base.helloagents;
+
+import org.arakhne.vmutil.locale.Locale;
+import org.janusproject.kernel.address.AgentAddress;
+import org.janusproject.kernel.agent.Agent;
+import org.janusproject.kernel.message.Message;
+import org.janusproject.kernel.message.StringMessage;
+import org.janusproject.kernel.status.Status;
+import org.janusproject.kernel.status.StatusFactory;
+
+/**
+ * A simple Agent waiting a message "hello" and sending a "welcome" message in
+ * return
+ * 
+ * @author $Author: ngaud$
+ * @version $Name$ $Revision$ $Date$
+ * @mavengroupid $GroupId$
+ * @mavenartifactid $ArtifactId$
+ */
+public class AgentB extends Agent {
+
+	private static final long serialVersionUID = -2478042408957087937L;
+
+	private boolean ACKsent = false;
+
+	@Override
+	public Status activate(Object... parameters) {
+		print(Locale.getString(AgentB.class, "AgentB.0")); //$NON-NLS-1$
+		return StatusFactory.ok(this);
+	}
+
+	@Override
+	public Status live() {
+
+		for (Message m : getMessages()) {
+			print(Locale.getString(AgentB.class, "AgentB.1")); //$NON-NLS-1$				
+			if (m instanceof StringMessage && ((StringMessage) m).getContent().equals(Launcher.WELCOME_MESSAGE_STRING_HEADER)) {
+				replyToMessage(m, new StringMessage(Launcher.WELCOME_MESSAGE_ACK_STRING_HEADER));
+				AgentAddress messageEmitter = (AgentAddress) m.getSender();
+				print(Locale.getString(AgentB.class, "AgentB.4", Launcher.WELCOME_MESSAGE_STRING_HEADER,messageEmitter)); //$NON-NLS-1$
+				print(Locale.getString(AgentB.class, "AgentB.5",Launcher.WELCOME_MESSAGE_ACK_STRING_HEADER)); //$NON-NLS-1$			
+				this.ACKsent = true;
+			}
+		}
+
+		if (this.ACKsent) {
+			print(Locale.getString(AgentB.class, "AgentB.6")); //$NON-NLS-1$
+			killMe();
+		}
+
+		return StatusFactory.ok(this);
+	}
+}
---
+++ janus/janus-demos/base-demos/helloagents/src/main/java/org/janusproject/demos/base/helloagents/osgi/HelloAgentsActivator.java
@@ -0,0 +1,152 @@
+/*
+ * $Id$
+ * 
+ * Janus platform is an open-source multiagent platform.
+ * More details on &lt;http://www.janus-project.org&gt;
+ * Copyright (C) 2013 Janus Core Developers
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+ */
+package org.janusproject.demos.base.helloagents.osgi;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.arakhne.vmutil.locale.Locale;
+import org.janusproject.demos.base.helloagents.AgentA;
+import org.janusproject.demos.base.helloagents.AgentB;
+import org.janusproject.demos.base.helloagents.Launcher;
+import org.janusproject.kernel.agent.KernelAgentFactory;
+import org.janusproject.kernel.logger.LoggerUtil;
+import org.janusproject.kernel.mmf.JanusApplication;
+import org.janusproject.kernel.mmf.KernelAuthority;
+import org.janusproject.kernel.mmf.KernelService;
+import org.janusproject.kernel.status.Status;
+import org.janusproject.kernel.status.StatusFactory;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author $Author: ngaud$
+ * @version $Name$ $Revision$ $Date$
+ * @mavengroupid $GroupId$
+ * @mavenartifactid $ArtifactId$
+ */
+public class HelloAgentsActivator implements BundleActivator, JanusApplication {
+
+	private Logger logger;
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		LoggerUtil.setGlobalLevel(Level.INFO);
+		this.logger = Logger.getLogger(this.getClass().getCanonicalName());
+		this.logger.info(Locale.getString(HelloAgentsActivator.class, "ACTIVATING_HELLOAGENT")); //$NON-NLS-1$
+		context.registerService(JanusApplication.class.getName(), this, null);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		//
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Status start(KernelService kernel) {
+		this.logger.log(Level.INFO, Locale.getString(HelloAgentsActivator.class, "HELLOAGENT_START")); //$NON-NLS-1$
+		
+		AgentA a = new AgentA();
+		AgentB b = new AgentB();
+
+		kernel.launchLightAgent(b, Locale.getString(Launcher.class, "AGENT-A")); //$NON-NLS-1$
+		kernel.launchLightAgent(a, Locale.getString(Launcher.class, "AGENT-B")); //$NON-NLS-1$
+		
+		return StatusFactory.ok(this);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public KernelAgentFactory getKernelAgentFactory() {
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public KernelAuthority getKernelAuthority() {
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isAutoStartJanusModules() {
+		return false;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Status stop(KernelService kernel) {
+		return StatusFactory.ok(this);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isStopOsgiFramework() {
+		return true;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isKeepKernelAlive() {
+		return false;
+	}
+
+	/** {@inheritDoc}
+	 */
+	@Override
+	public String getName() {
+		return Locale.getString(HelloAgentsActivator.class, "APPLICATION_NAME"); //$NON-NLS-1$
+	}
+
+	/** {@inheritDoc}
+	 */
+	@Override
+	public String getDescription() {
+		return Locale.getString(HelloAgentsActivator.class, "APPLICATION_DESCRIPTION"); //$NON-NLS-1$
+	}
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isRunning() {
+		return true;
+	}
+}
