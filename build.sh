0a47e821226ef72be4f5b0aad1cf3535a0d92cd6
sgalland <stephane.galland@utbm.fr>
2013-05-23 15:02:56 +0200

    * Prepare for Release 0.6: Add global building scripts for the Janus platform and the Eclipse modules.

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Get the directy where this script is installed
+DIRNAME=`dirname $0`
+DIRNAME=`readlink -f "$DIRNAME"`
+
+# Detect the java
+if test -n "$JAVA_HOME"
+then
+	echo "JAVA_HOME=$JAVA_HOME"
+	JAVA="$JAVA_HOME/bin/java"
+else
+	JAVA="java"
+fi
+
+# Check for the right version of Java
+JAVA_VERSION=`$JAVA -version 2>&1 | awk '/version/ {print $3}' | egrep -o '[0-9]+\.[0-9]+'`
+
+echo "Version=$JAVA_VERSION"
+
+if test "v$JAVA_VERSION" != "v1.6"
+then
+	echo "ERROR: you must use a JDK 1.6 to compile the Janus platform," >&2
+	echo "ERROR: because several parts of the platform are not compatible " >&2
+	echo "ERROR: newver versions (eg. Android modules)." >&2
+	echo "ERROR: Recommended command line:" >&2
+	echo "ERROR: $> JAVA_HOME=\"path/to/jdk/1.6\" ./build.sh" >&2
+	exit 1
+fi
+
+# Compile
+cd "$DIRNAME/janus"
+mvn -Dmaven.test.skip=true clean install
+cd "$DIRNAME/janus-ui"
+mvn -Dmaven.test.skip=true clean install
+
+exit 0
---
+++ clean.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+
+for /f "tokens=3" %%g in ('java -version 2^>^&1 ^| findstr /i "version"') do (
+    echo Output: %%g
+    set JAVAVER=%%g
+)
+set JAVAVER=%JAVAVER:"=%
+
+for /f "delims=. tokens=1-3" %%v in ("%JAVAVER%") do (
+    set JAVAMAJORVER=%%v
+    set JAVAMINORVER=%%w
+    set JAVABUILDVER=%%x
+)
+
+if "%JAVAMAJORVER%"=="1" goto step1:
+goto errorcase
+
+:step1
+if "%JAVAMINORVER%"=="6" goto step2:
+goto errorcase
+
+:step2
+cd %~dp0\janus
+mvn -Dmaven.test.skip=true clean
+cd %~dp0\janus-ui
+mvn -Dmaven.test.skip=true clean
+exit 0
+
+:errorcase
+echo "ERROR: you must use a JDK 1.6 to compile the Janus platform,"
+echo "ERROR: because several parts of the platform are not compatible "
+echo "ERROR: newver versions (eg. Android modules)."
+exit 1
+
